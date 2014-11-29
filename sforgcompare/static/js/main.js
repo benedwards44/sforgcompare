f9f54cfd9634a5a4ff929574f06da7d2393131f1
Ben Edwards <bedwards@tquila.com>
2014-11-29 15:06:14 -0800

    Getting there

--- sforgcompare/static/js/main.js
+++ sforgcompare/static/js/main.js
@@ -1,51 +1,66 @@
-$(document).ready(function() {
+$(document).ready(function ()
+{
+	$('#orgOneButton').click(function()
+	{
+		var loginUrl = 'https://';
+		if ( $('#org_one_env').val() == 'Production' )
+		{
+			loginUrl += 'login'
+		}
+		else
+		{
+			loginUrl += 'test'
+		}
+		loginUrl += '.salesforce.com/services/oauth2/authorize?response_type=code&client_id={{client_id}}&redirect_uri={{redirect_uri}}&scope=api&state=' + $('#org_one_env').val() + 'org1';
+		popupwindow(loginUrl);
+	});
 
-	$('.loading').hide();
+	$('#orgTwoButton').click(function()
+	{
+		var loginUrl = 'https://';
+		if ( $('#org_two_env').val() == 'Production' )
+		{
+			loginUrl += 'login'
+		}
+		else
+		{
+			loginUrl += 'test'
+		}
+		loginUrl += '.salesforce.com/services/oauth2/authorize?response_type=code&client_id={{client_id}}&redirect_uri={{redirect_uri}}&scope=api&state=' + $('#org_one_env').val() + 'org2';
+		popupwindow(loginUrl);
+	});
 
-	$('.logging_out').hide();
-
-	/* Scroll to top logic */
-	var offset = 220;
-    var duration = 500;
-    $(window).scroll(function() {
-        if (jQuery(this).scrollTop() > offset) {
-            jQuery('.back-to-top').fadeIn(duration);
-        } else {
-            jQuery('.back-to-top').fadeOut(duration);
-        }
-    });
-    
-    $('.back-to-top').click(function(event) {
-        event.preventDefault();
-        jQuery('html, body').animate({scrollTop: 0}, duration);
-        return false;
-    });
-	
 });
 
-function hideTable(){
-	$('.login_table').hide();
-	$('#select_components').hide();
-	$('.messages').hide();
-	$('.loading').show();
-}
-function showTable(){
-	$('.login_table').show();
-	$('#select_components').show();
-	$('.messages').show();
-	$('.loading').hide();
-}
+function popupwindow(url) 
+{
+	var w = 800;
+	var h = 500;
+  	var left = (screen.width/2)-(w/2);
+  	var top = (screen.height/2)-(h/2);
+  	return window.open(url, 'SalesforceLogin', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
+} 
 
-function showLogout(){
-	$('.login_table').hide();
-	$('#select_components').hide();
-	$('.messages').hide();
-	$('.logging_out').show();
-}
+function updateOrgDetails(org, username, org_name)
+{
+	if (org == 'one')
+	{
+		$('#orgOneLogin').hide();
+		$('#orgOneDisplay').show();
+		$('#orgOneUsername').text(username);
+		$('#orgOneOrg').show(org_name);
+	}
+	else
+	{
+		$('#orgTwoLogin').hide();
+		$('#orgTwoDisplay').show();
+		$('#orgTwoUsername').text(username);
+		$('#orgTwoOrg').show(org_name);
+	}
 
-function hideLogout(){
-	$('.login_table').show();
-	$('#select_components').show();
-	$('.messages').show();
-	$('.logging_out').hide();
+	// Both elements are visible, show the GO button
+	if ( $('orgOneDisplay').is(":visible") && $('orgTwoDisplay').is(":visible") )
+	{
+		$('#compareOrgs').show();
+	}
 }
