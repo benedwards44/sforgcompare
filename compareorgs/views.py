870bd90f91123c79d88c0cea598529539b33ce1c
Ben Edwards <bedwards@tquila.com>
2014-11-28 19:07:35 -0800

    Making progress yo

--- compareorgs/views.py
+++ compareorgs/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.http import HttpResponse, HttpResponseRedirect
-from compareorgs.forms import LoginForm
 from django.conf import settings
 import json	
 import requests
@@ -9,6 +8,7 @@ from time import sleep
 
 def index(request):
 
-	login_form = LoginForm()
+	client_id = settings.SALESFORCE_CONSUMER_KEY
+	redirect_uri = settings.SALESFORCE_REDIRECT_URI
 
-	return render_to_response('index.html', RequestContext(request,{'login_form': login_form}))
+	return render_to_response('index.html', RequestContext(request,{'client_id': client_id, 'redirect_uri': redirect_uri}))
