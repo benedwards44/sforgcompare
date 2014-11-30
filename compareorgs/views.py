c054fe3304c77580a933ae52ae29372baa63bd74
Ben Edwards <bedwards@tquila.com>
2014-11-29 18:21:13 -0800

    Missing import

--- compareorgs/views.py
+++ compareorgs/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.http import HttpResponse, HttpResponseRedirect
 from django.conf import settings
+from compareorgs.models import Org
 import json	
 import requests
 from time import sleep
