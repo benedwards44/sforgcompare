9d941a5ebee9975dab69ad309d2f15324676a397
Ben Edwards <bedwards@tquila.com>
2014-11-29 16:46:07 -0800

    Style updates

--- sforgcompare/static/js/main.js
+++ sforgcompare/static/js/main.js
@@ -36,8 +36,8 @@ $(document).ready(function ()
 
 function popupwindow(url) 
 {
-	var w = 800;
-	var h = 500;
+	var w = 600;
+	var h = 300;
   	var left = (screen.width/2)-(w/2);
   	var top = (screen.height/2)-(h/2);
   	return window.open(url, 'SalesforceLogin', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
