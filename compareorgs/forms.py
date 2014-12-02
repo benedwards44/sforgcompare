8339f5bbaf42af8ebfb15fddad0028f1f14c5bcd
Ben Edwards <bedwards@tquila.com>
2014-12-01 18:39:59 -0800

    Add API choice options

--- compareorgs/forms.py
+++ compareorgs/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 
 class JobForm(forms.Form):
-	org_one = forms.IntegerField(required=False);
+	org_one = forms.IntegerField(required=False)
 	org_two = forms.IntegerField(required=False)
+	api_choice = forms.CharField()
