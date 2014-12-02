5ec48526d0ed812c7b23df673ecb47de9644c21e
Ben Edwards <bedwards@tquila.com>
2014-12-01 18:37:34 -0800

    Forced HTTPS

--- sforgcompare/settings.py
+++ sforgcompare/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'sslify.middleware.SSLifyMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
