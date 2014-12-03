71630649cc7073f9da61de04ee33caa2afa62f56
Ben Edwards <bedwards@tquila.com>
2014-12-02 17:48:34 -0800

    Typo

--- compareorgs/admin.py
+++ compareorgs/admin.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 from compareorgs.models import Job, Org
 
 class OrgInline(admin.TabularInline):
-	fields = ['org_number','org_name', 'username', 'status', 'error']
+	fields = ['org_number','org_name', 'username', 'access_token', 'status', 'error']
 	ordering = ['org_number']
 	model = Org
 	extra = 0
 
 class JobAdmin(admin.ModelAdmin):
-    list_display = ('created_date','access_token','username','status','error')
+    list_display = ('created_date','status','error')
     inlines = [OrgInline]
 
 admin.site.register(Job, JobAdmin)
