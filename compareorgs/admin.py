d8206d1b6579ea42e44cab75c6dacf8cbeedfa14
Ben Edwards <bedwards@tquila.com>
2014-12-02 22:41:00 -0800

    Typo

--- compareorgs/admin.py
+++ compareorgs/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from compareorgs.models import Job, Org
+from compareorgs.models import Job, Org, Component, ComponentType
 
 class OrgInline(admin.TabularInline):
-	fields = ['org_number','org_name', 'username', 'access_token', 'status', 'error']
+	fields = ['id','org_number','org_name', 'username', 'access_token', 'status', 'error']
 	ordering = ['org_number']
 	model = Org
+	extra = 0
+
+class ComponentTypeInline(admin.TabularInline):
+	fields = ['id','name', 'content']
+	ordering = ['name']
+	model = ComponentType
 	extra = 0
 
 class JobAdmin(admin.ModelAdmin):
