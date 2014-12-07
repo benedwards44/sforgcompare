bf8cadb05ba3846fdc3981cb7d71e7b291b55611
Ben Edwards <bedwards@tquila.com>
2014-12-07 19:53:48 +0000

    Stuff

--- compareorgs/tasks.py
+++ compareorgs/tasks.py
@@ -347,15 +347,21 @@ def compare_orgs_task(job):
 					# Files differ - time to compare
 					else:
 
+						diff_tool = HtmlDiff()
+						diff_html = diff_tool.make_table(component_map['left' + row_value].content, component_map['right' + row_value].content)
+
 						html_output += '<tr class="component danger component_' + row_value.split('.')[0] + '">'
 						html_output += '<td id="' + row_value + '" class="diff">'
 						html_output += row_value.split('.')[1]
+						html_output += '<textarea style="display:none;">' +  diff_html + '</textarea>'
 						html_output += '</td>'
 						html_output += '<td id="' + row_value + '" class="diff">'
 						html_output += row_value.split('.')[1]
-						html_output += '<textarea style="display:none;">' +  component_map['right' + row_value].content + '</textarea>'
+						html_output += '<textarea style="display:none;">' +  diff_html + '</textarea>'
 						html_output += '</td>'
 						html_output += '</tr>'
+
+						diff_htm
 
 		html_output += '</tbody>'
 		html_output += '</table>'
