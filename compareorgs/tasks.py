225150f7feea60cccf484c39603f5c37aed27075
Ben Edwards <bedwards@tquila.com>
2014-12-15 17:26:39 +0000

    Stuff

--- compareorgs/tasks.py
+++ compareorgs/tasks.py
@@ -335,7 +335,7 @@ def compare_orgs_task(job):
 						print 'Start Diff'
 
 						diff_tool = HtmlDiff()
-						diff_html = diff_tool.make_table(component_map['left' + row_value].content.split('\n'), component_map['right' + row_value].content.split('\n'))
+						diff_html = diff_tool.make_table(component_map['left' + row_value].content.split('\n'), component_map['right' + row_value].content.split('\n'), context=True)
 
 						html_output += '<tr class="component warning component_' + row_value.split('.')[0] + '">'
 						html_output += '<td id="' + row_value + '" class="diff">'
