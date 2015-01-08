86982510e705550fd648f79bc7bf4885db7395bb
Ben Edwards <bedwards@tquila.com>
2015-01-08 14:54:14 +0000

    Fix models

--- compareorgs/models.py
+++ compareorgs/models.py
@@ -54,8 +54,8 @@ class Component(models.Model):
 class ComponentListUnique(models.Model):
 	job = models.ForeignKey(Job)
 	component_type_left = models.ForeignKey(ComponentType, related_name='component_unique_left')
-	component_left = models.ForeignKey(Component, related_name='component_left')
+	component_left = models.ForeignKey(Component, related_name='component_left', blank=True, null=True)
 	component_type_right = models.ForeignKey(ComponentType, related_name='component_unique_right')
-	component_right = models.ForeignKey(Component, related_name='component_right')
+	component_right = models.ForeignKey(Component, related_name='component_right', blank=True, null=True)
 	diff = models.BooleanField()
 	diff_html = models.TextField(blank=True, null=True)
