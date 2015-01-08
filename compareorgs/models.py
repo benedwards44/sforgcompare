b6618022e9933d98224a832768d317e166dfadd6
Ben Edwards <bedwards@tquila.com>
2015-01-08 12:38:41 +0000

    Stuff

--- compareorgs/models.py
+++ compareorgs/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Job(models.Model):
 	created_date = models.DateTimeField(null=True,blank=True)
 	finished_date = models.DateTimeField(null=True,blank=True)
+	password = models.CharField(max_length=255, blank=True)
 	api_choice = models.CharField(max_length=255,blank=True)
 	email_result = models.BooleanField()
 	email = models.CharField(max_length=255,blank=True)
