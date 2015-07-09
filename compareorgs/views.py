a17327705a0c08c683f16435e4fdc96c99b03743
Ben Edwards <bedwards@tquila.com>
2015-07-09 16:03:30 +0100

    Stuff

--- compareorgs/views.py
+++ compareorgs/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render_to_response, get_object_or_404
-from django.template import RequestContext
-from django.template import Context, Template
+from django.template import RequestContext, Context, Template, loader
 from django.http import HttpResponse, HttpResponseRedirect
 from django.conf import settings
 from compareorgs.models import Job, Org, ComponentType, Component, ComponentListUnique
