5ca5bbde1b466f921dbe445ded3fe51fb7cccbd2
Ben Edwards <bedwards@tquila.com>
2015-07-27 16:23:57 +0100

    Stuff

--- compareorgs/tasks.py
+++ compareorgs/tasks.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from celery import Celery
 import os
 import json	
+import ast
 import requests
 import datetime
 import time
@@ -434,11 +435,11 @@ def create_offline_file(job, offline_job):
 
 		# Create JSON files
 		component_diff_json = open(temp_dir_string + 'component_diffs.json','w+')
-		component_diff_json.write('var diff_data = ' + str(component_diff_records) + ';')
+		component_diff_json.write('var diff_data = ' + json.dumps(component_diff_records) + ';')
 		component_diff_json.close()
 
 		component_json = open(temp_dir_string + 'components.json','w+')
-		component_json.write('var component_data = ' + str(component_records) + ';')
+		component_json.write('var component_data = ' + json.dumps(component_records) + ';')
 		component_json.close()
 
 		# Create html file
