110399ef5767f02c864167cfdae6d2192e4a1520
Jan Aertgeerts <jan@veno.be>
2016-05-20 23:47:31 +0200

    upgrade django + settings

--- sforgcompare/settings.py
+++ sforgcompare/settings.py
@@ -82,7 +82,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
