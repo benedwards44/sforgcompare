1d9f9097353d0897391aa7674a97ddbf276bd24e
Jan Aertgeerts <jan@veno.be>
2016-05-20 23:54:10 +0200

    changed collect static dir

--- sforgcompare/settings.py
+++ sforgcompare/settings.py
@@ -85,11 +85,6 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
 ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 
